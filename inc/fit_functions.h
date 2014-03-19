c39a9e280f8a7a6df88c770927f80af0e361b2d2
Vladyslav <libov@mail.desy.de>
2014-03-19 15:23:19 +0100

    added normalised gauss declaration

--- inc/fit_functions.h
+++ inc/fit_functions.h
@@ -4,6 +4,7 @@
 #include<TROOT.h>
 
 Double_t gauss(Double_t *x, Double_t *par);
+Double_t normalised_gauss(Double_t *x, Double_t *par);
 Double_t breit_wigner(Double_t *x, Double_t *par);
 
 Double_t pol3(Double_t *x, Double_t *par);
