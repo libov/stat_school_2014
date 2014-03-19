c8189d2216c38cbce819d310cc15497f69fb34ae
Vladyslav <libov@mail.desy.de>
2014-03-19 14:38:11 +0100

    added declaration of several lookup-table functions

--- inc/fit_functions.h
+++ inc/fit_functions.h
@@ -10,4 +10,10 @@ Double_t pol3(Double_t *x, Double_t *par);
 
 TString get_parameter_name(TString function, unsigned par_nr);
 
+unsigned get_n_parameters(TString function);
+
+typedef Double_t (*FIT_FUNCTION)(Double_t *x, Double_t *par);
+
+FIT_FUNCTION get_function_pointer(TString function); // another way: Double_t (*get_function_pointer(TString function))(Double_t *x, Double_t *par);
+
 #endif
