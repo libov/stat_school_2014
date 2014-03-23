cd2e1e2563c40f3a5226349e7f9671316c895a25
Vladyslav Libov <vladyslav@linux-mebx>
2014-03-23 10:54:16 +0100

    removed blank lines

--- src/generate_data.cxx
+++ src/generate_data.cxx
@@ -158,8 +158,7 @@ int main (int argc, char **argv) {
                     h -> Fill(x);
                     j++;
                 }
-            }
-            
+            }            
         }
     }
 
