a1ccad291b2e3d8d2939aa58c2dac95c7ee070fe
Vladyslav <libov@mail.desy.de>
2014-03-24 08:22:41 +0100

    removed setting the global pointer from the main function, moved to the class constructor

--- src/mass_peak_fit.cxx
+++ src/mass_peak_fit.cxx
@@ -77,7 +77,6 @@ int main (int argc, char **argv) {
 
     // create object
     TMassPeakFit instance(config, minimization_function);
-    gTMassPeakFit = &instance;
 
     if (custom_fit) {
         instance.CustomFit();
