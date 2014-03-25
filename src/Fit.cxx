147fca0e36a2952fc4a281ca50f8a86065b7228d
Vladyslav Libov <vladyslav@linux-mebx>
2014-03-25 18:57:03 +0100

    removed initialisation from Fit

--- src/Fit.cxx
+++ src/Fit.cxx
@@ -30,19 +30,6 @@ using namespace std;
 // --------------------------------------------------------- //
 void TMassPeakFit::Fit() {
 
-    // initialise Minuit (set parameter names, start values, limits, fit steps; some of these are set in the config file, others directly here)
-    Double_t fitStep[99], limitMin[99], limitMax[99];
-    for (int i=0; i<99; i++) {
-        fitStep[i]  = 0.01;
-        limitMin[i] = 0;
-        limitMax[i] = 0;
-    }
-
-    for (unsigned i=0;i<fNParameters;i++) {
-        fMinuit->DefineParameter(i, fParName[i], fStartValues[i], fitStep[i], limitMin[i], limitMax[i]);
-        if ( fFixParameters[i] == 1 ) fMinuit -> FixParameter(i);
-    }
-
     // if only_draw_initial is 1 in the config file, the minimization is not performed - instead function using start values are displayed
     if (fOnlyDrawInitial) return;
 
